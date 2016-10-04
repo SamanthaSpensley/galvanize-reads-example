5bb0568a401b69c70f160bf0c5e0b5907e6c6292
Samantha Spensley <spenslsm@gmail.com>
2016-10-03 18:58:28 -0600

    add new book

--- db/query.js
+++ db/query.js
@@ -4,6 +4,16 @@ function Books() {
   return knex('book');
 }
 
+function AddBook(title, genre, description, cover) {
+  return knex('book').insert({
+    title:title,
+    genre:genre,
+    description:description,
+    cover:cover,
+  })
+}
+
 module.exports = {
   getAllBooks: Books,
+  addBook: AddBook,
 }
--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ var query = require('../db/query');
 var knex = require('../db/knex');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   query.getAllBooks()
   .then(function(book) {
     res.render('list_books', {
@@ -12,5 +12,16 @@ router.get('/', function(req, res, next) {
     })
   })
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('add_book')
+});
+
+router.post('/', (req, res, next) => {
+  query.addBook(req.body.title, req.body.genre, req.body.description, req.body.cover)
+  .then(() => {
+    res.redirect('/books')
+  })
+})
 
 module.exports = router;
